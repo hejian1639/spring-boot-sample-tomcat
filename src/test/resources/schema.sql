0b217472efa63dc2b26160479a5c03575a4f654b
何健 <john.he@fairlinkcentury.com>
2017-06-16 13:43:26 +0800

    add mapper test

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,31 @@
+--
+--    Copyright 2010-2013 the original author or authors.
+--
+--    Licensed under the Apache License, Version 2.0 (the "License");
+--    you may not use this file except in compliance with the License.
+--    You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--    Unless required by applicable law or agreed to in writing, software
+--    distributed under the License is distributed on an "AS IS" BASIS,
+--    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--    See the License for the specific language governing permissions and
+--    limitations under the License.
+--
+
+
+drop table if exists account;
+
+create table account (
+    user_name varchar(64) not null,
+    email varchar(64) null,
+    primary key (user_name)
+);
+
+
+drop table if exists city;
+
+create table city (id int primary key auto_increment, name varchar, state varchar, country varchar);
+
+insert into city (name, state, country) values ('San Francisco', 'CA', 'US');
